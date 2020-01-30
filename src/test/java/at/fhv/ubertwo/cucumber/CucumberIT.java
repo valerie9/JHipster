8f50c71230c8608819255ed977312beb3bdbe966
valer <valerie@gsiberg.com>
2020-01-30 11:03:10 +0100

    added ignore for cucumber tests

--- src/test/java/at/fhv/ubertwo/cucumber/CucumberIT.java
+++ src/test/java/at/fhv/ubertwo/cucumber/CucumberIT.java
@@ -1,10 +1,12 @@
 package at.fhv.ubertwo.cucumber;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
+@Ignore
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty", features = "src/test/features")
 public class CucumberIT  {
